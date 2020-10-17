ab7ccfb624d1a5477cad6974064cb96af7d23696
Tatsiana Zhylevich <rocktania@mail.ru>
2020-10-18 01:17:26 +0300

    add button myWeather and function getMyWeather

--- weather-app/index.js
+++ weather-app/index.js
@@ -1,7 +1,7 @@
 let input = document.querySelector('.input');
 let form = document.getElementById('form');
 let buttonClear = document.getElementById('clear');
-
+let buttonMyWeather = document.getElementById('myWeather');
 let tableBody = document.getElementById('tbody');
 let cityMap = document.querySelector('.cityMap');
 
@@ -11,7 +11,7 @@ function renderTableRow() {
 
     let tBody = '';
     let cMap = '';
-    map.forEach((value, key) => {
+    map.forEach((value) => {
         const[city, country, temp, lat, lon] = value;
         tBody += `
         <tr>
@@ -20,7 +20,6 @@ function renderTableRow() {
             <td>${temp}</td>
             <td>${temp}</td>
         </tr>
-       
         `
         cMap += `<img src='https://maps.locationiq.com/v2/staticmap?key=pk.c6c8e0f26d559c4194d567e972e29b74&center=${lat},${lon}&zoom=12&size=400x400&markers=${lat},${lon}|icon:large-red-cutout'></img>`
     })
@@ -30,26 +29,43 @@ function renderTableRow() {
 
 function getWeather(city){
     return fetch(`http://api.weatherstack.com/current?access_key=8a2380626129e4b4b0b74c8703d3d9c3&query=${city}`)
-    .then(response => response.json());
+    .then(response => response.json())
+    .then((response) => {
+        const{
+            current: {temperature},
+            location: {country, name, lat, lon},
+        } = response;
+        map.set(name, [name, country, temperature, lat, lon])
+       renderTableRow();
+    })  
 }
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
-
-    getWeather(input.value)
-    .then((response) => {
-        console.log(response);
-        const{
-            current: {temperature},
-            location: {country, name, lat, lon},
-
-        } = response;
-        map.set(name, [name, country, temperature, lat, lon])
-       
-       renderTableRow();
-    })  
+    getWeather(input.value);
 })
 
+function getMyWeather({coords: {latitude: lat, longitude: lon}}){
+    console.log(`${lat}, ${lon}`)
+    fetch(`https://eu1.locationiq.com/v1/reverse.php?key=pk.80f74496452169d08d8ab400676ea8d9&lat=${lat}&lon=${lon}&format=json&accept-language=en`)
+    .then(response => response.json())
+    .then((response) => {
+        getWeather(response.address.town)
+    })
+}
+
+function getMyLocation() {
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(getMyWeather);
+    }else{
+        alert('Ваш браузер не поддерживает Geolocation API'); 
+    }
+}
+
+buttonMyWeather.addEventListener('click', () => {
+    getMyLocation()
+   
+});
 
 buttonClear.addEventListener('click', () => {
     map.clear();
